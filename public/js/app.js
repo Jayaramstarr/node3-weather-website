878f3a6d7549ca6a6674286695555a1a4291b838
unknown <jayaramstarr33@gmail.com>
2020-04-03 14:23:21 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ addEventListener('submit',(e)=>{
 
     const location = search.value;
     
-    fetch(`http://localhost:3000/weather?address=${location}`).then((res)=>{
+    fetch(`/weather?address=${location}`).then((res)=>{
         res.json().then((data)=>{
             if(data.error)
                 return msg1.textContent = data.error;
